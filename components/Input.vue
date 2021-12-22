8613f7e9e99f0dff2c17b1b0f50d8cf8a5890dc5
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2021-12-22 15:39:50 +0100

    feat: custom input component

---
+++ components/Input.vue
@@ -0,0 +1,35 @@
+<template>
+  <input
+    :value="value"
+    type="text"
+    :name="name"
+    class="input"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.input {
+  width: 100%;
+  border: none;
+  padding: 20px 20px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.16);
+  border-radius: 4px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>Bravado Task</h1>
+  <div class="app">
+    <div class="container">
+      <Input v-model="query" name="search" />
+    </div>
+  </div>
 </template>
 
 <script>
+import Input from '~/components/Input'
 export default {
   name: 'IndexPage',
+  components: {
+    Input,
+  },
+  data() {
+    return {
+      query: '',
+    }
+  },
 }
 </script>
+<style lang="scss" scoped>
+.app {
+  background-color: $grey;
+  width: 100%;
+  min-height: 100vh;
+  padding: 40px 0;
+}
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
