50daeaa945e89936c5ec59982262605d6c28b4ed
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2021-12-22 15:52:11 +0100

    feat: debounce filter search value

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,49 @@
   <div class="app">
     <div class="container">
       <Input v-model="query" name="search" />
+      <Card
+        v-for="(user, i) in filteredUsers"
+        :key="user.email + i"
+        :item="user"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import debounce from 'lodash.debounce'
 import Input from '~/components/Input'
+import Card from '~/components/Card'
+import users from '~/static/users.json'
+
 export default {
   name: 'IndexPage',
   components: {
     Input,
+    Card,
   },
   data() {
     return {
       query: '',
+      users,
+      filteredUsers: [],
     }
+  },
+  watch: {
+    query(value) {
+      this.debounceInput(value)
+    },
+  },
+  methods: {
+    debounceInput: debounce(function (query) {
+      const filtered = users?.filter((obj) =>
+        Object.entries(obj).some(
+          ([key, value]) =>
+            key !== 'avatar' && String(value).toLowerCase().includes(query)
+        )
+      )
+      this.filteredUsers = filtered.slice(0, 5)
+    }, 1000),
   },
 }
 </script>
@@ -30,5 +58,7 @@ export default {
 .container {
   max-width: 800px;
   margin: 0 auto;
+  max-height: 700px;
+  overflow-y: scroll;
 }
 </style>
